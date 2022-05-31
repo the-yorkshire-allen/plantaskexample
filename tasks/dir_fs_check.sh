a51a236c2002ceec7d51b1499920749f3413357a
root <root@pe-server-60e8e9-0.us-west1-a.c.emea-ps-lab.internal>
2022-05-31 14:45:33 +0000

    Added json parameter

--- tasks/dir_fs_check.sh
+++ tasks/dir_fs_check.sh
@@ -10,8 +10,12 @@ success() {
    echo "${1}"
 }
 
-escape() {
-   printf '%s' "$1" | sed -E ':a;N;$!ba;s/\r{0,1}\n/\\n/g;s/\"/\\"/g;s/\t/\\t/g'
+format() {
+   if [ ${PT_json} -eq "true" ]; then
+      printf \"%s\"' "$1" | sed -E ':a;N;$!ba;s/\r{0,1}\n/\\n/g;s/\"/\\"/g;s/\t/\\t/g'
+   else
+      printf '\n\"%s\"\n' "%1"
+   fi
 }
 
 if [[ $# -eq 1 ]]; then
@@ -24,16 +28,16 @@ if [ ! -d "${PT_directory}" ]; then
 fi
 
 # declare variables
-dir_df=$(escape "$(df -Ph ${PT_directory})")
-kernel_rpms=$(escape "$(rpm -qa | grep ^kernel-[1-4])")
-directory_files=$(escape "$(ls -al ${PT_directory})")
-directory_usage=$(escape "$(du -a -d 1 ${PT_directory} | sort -n)")
+dir_df=$(format "$(df -Ph ${PT_directory})")
+kernel_rpms=$(format "$(rpm -qa | grep ^kernel-[1-4])")
+directory_files=$(format "$(ls -al ${PT_directory})")
+directory_usage=$(format "$(du -a -d 1 ${PT_directory} | sort -n)")
 percentage_used="$(df -Ph ${PT_directory} | awk 'NR==2 {print substr($5, 1, length($5)-1)}')"
 
-success "{\"percentage_used\":\"${percentage_used}\", \
-          \"dir_df\":\"${dir_df}\", \
-          \"kernel_rpms\":\"${kernel_rpms}\", \
-          \"directory_files\":\"${directory_files}\", \
-          \"directory_usage\":\"${directory_usage}\"}"
+success "{\"percentage_used\":${percentage_used}, \
+          \"dir_df\":${dir_df}, \
+          \"kernel_rpms\":${kernel_rpms}, \
+          \"directory_files\":${directory_files}, \
+          \"directory_usage\":${directory_usage}}"
 
 exit 0
